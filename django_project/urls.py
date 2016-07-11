92bc31b2640b44048981e4ea05a6ccac2a41f9d3
dusty <dpet123@yahoo.com>
2016-07-10 21:50:55 -0400

    update home

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,5 +15,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
     url(r'^contact/', views.contact, name='contact'),
+    url(r'^help/', views.help, name='help'),
+    url(r'^signup/', views.signup, name='signup'),
+    url(r'^login/', views.login, name='login'),
 
 ]

d6fb40b9eedbb31811889b997a9aed33f804969f
hermstadalex <hermstadalex@gmail.com>
2016-07-22 16:42:08 -0700

    Test:

--- django_project/forms.py
+++ django_project/forms.py
@@ -3,6 +3,10 @@ from django import forms
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
 
+
 class ArtistForm(forms.Form):
+    """
+        Form for artist name on front page.
+    """
     artist_name = forms.CharField(label='', max_length=100,
             widget=forms.TextInput(attrs={'class': "form-control"}))

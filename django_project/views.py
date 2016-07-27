334e0a793f482d4ce0059978f8f54c17d1cb52e6
dpet123 <dpet123@yahoo.com>
2016-07-27 22:40:45 +0000

    added similar artists to views.py and results_page.html

--- django_project/views.py
+++ django_project/views.py
@@ -56,14 +56,19 @@ def formtest_start(request):
 def results_page(request):
 
     artist = request.POST.items()[1][1] 
+    similarArtist= request.POST.items()[1][1]
 
     url = 'http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=' + artist + '&api_key=673c370b7184a43691df1c7c2b35874f&format=json'
-
+    url2 = 'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=' + similarArtist + '&api_key=673c370b7184a43691df1c7c2b35874f&format=json'
     r = requests.get( url )
+    s = requests.get(url2 )
 
     top_albums = []
-
+    simArtist = []
+    
     for i in range(5):
         top_albums.append(r.json()["topalbums"]["album"][i]["name"])
-
-    return render(request, 'results_page.html', {'top_albums': top_albums} )
+    for j in range(3):
+        simArtist.append(s.json()["similarartists"]["artist"][j]["name"])
+    
+    return render(request, 'results_page.html', {'top_albums': top_albums, 'simArtist': simArtist},) 
